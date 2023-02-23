f505874233d690f771b42ea3f76d10f5f1d73eec
VKSingh03 <vsingh03@umd.edu>
2023-02-23 08:21:38 -0500

    First commit

---
+++ include/group7/competition_ready_subscriber.hpp
@@ -0,0 +1,25 @@
+# include <memory>
+# include "rclcpp/rclcpp.hpp"
+# include <ariac_msgs/msg/competition_state.hpp>
+# include <ariac_msgs/msg/order.hpp>
+# include <ariac_msgs/srv/submit_order.hpp>
+#include <std_srvs/srv/trigger.hpp>
+
+class competition_ready_subscriber:public rclcpp::Node
+{
+
+public:
+    competition_ready_subscriber(std::string node_name):Node(node_name)
+    {
+        competition_state_subscriber = this->create_subscription<ariac_msgs::msg::CompetitionState>("/ariac/competition_state", 10, std::bind(&competition_ready_subscriber::competition_state_callback, this, std::placeholders::_1));
+    }
+
+private:
+    // attributes
+    rclcpp::Subscription<ariac_msgs::msg::CompetitionState>::SharedPtr competition_state_subscriber;
+    // ariac_msgs::msg::CompetitionState msg;
+    // rclcpp::Client<std_srvs::srv::Trigger>::ConstSharedPtr client;
+
+    // methods
+    void competition_state_callback(const ariac_msgs::msg::CompetitionState::SharedPtr msg);
+};
